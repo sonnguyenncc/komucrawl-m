6cb2d3c36f61b287353b6d9845ba979f49160bc0
Nhan Nguyen <nguyentran@ncc.asia>
2024-08-21 17:12:33 +0700

    new version

--- index.js
+++ index.js
@@ -358,15 +358,15 @@ client.onchannelmessage = async (msg) => {
       return;
     }
 
+     await pgclient.query("INSERT INTO komu_daily(userid, email, daily, channelid) VALUES ($1, $2, $3, $4) RETURNING *", 
+      [msg.sender_id, msg.username, ref.content, msg.channel_id]).catch(err => console.log(err));
+
     if (!checkTimeSheet()) {
       await client.sendMessage(msg.clan_id, msg.channel_id, msg.mode, {"t": "✅ Daily saved. (Invalid daily time frame. Please daily at 7h30-9h30, 12h-18h. WFH not daily 20k/time.)"}, undefined, undefined, Array(ref));
     } else if (!checkTimeNotWFH()) {
       await client.sendMessage(msg.clan_id, msg.channel_id, msg.mode, {"t": "✅ Daily saved. (Invalid daily time frame. Please daily at 7h30-17h. not daily 20k/time.)"}, undefined, undefined, Array(ref));
+    } else {
+      await client.sendMessage(msg.clan_id, msg.channel_id, msg.mode, message, undefined, undefined, Array(ref));
     }
-            
-    await pgclient.query("INSERT INTO komu_daily(userid, email, daily, channelid) VALUES ($1, $2, $3, $4) RETURNING *", 
-      [msg.sender_id, msg.username, ref.content, msg.channel_id]).catch(err => console.log(err));
-
-    await client.sendMessage(msg.clan_id, msg.channel_id, msg.mode, message, undefined, undefined, Array(ref));
   }
 }
