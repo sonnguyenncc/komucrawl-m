7f2c3efc365f51e78c35194ae150b773b29cc0b9
man2807 <73380926+man2807@users.noreply.github.com>
2024-09-17 14:17:12 +0700

    add ncc8 command (#11)
    Co-authored-by: man.maihong <man.maihong@ncc.asia>

--- src/bot/asterisk-commands/commands/ncc8/ncc8.commnad.ts
+++ src/bot/asterisk-commands/commands/ncc8/ncc8.commnad.ts
@@ -1,16 +1,20 @@
-import { ChannelMessage } from 'mezon-sdk';
+import { ChannelMessage, MezonClient } from 'mezon-sdk';
 import { Command } from 'src/bot/base/commandRegister.decorator';
 import { CommandMessage } from '../../abstracts/command.abstract';
 import { ClientConfigService } from 'src/bot/config/client-config.service';
 import { AxiosClientService } from 'src/bot/services/axiosClient.services';
+import { MezonClientService } from 'src/mezon/services/client.service';
 
 @Command('ncc8')
 export class Ncc8Command extends CommandMessage {
+  private client: MezonClient;
   constructor(
     private clientConfigService: ClientConfigService,
     private axiosClientService: AxiosClientService,
+    private clientService: MezonClientService,
   ) {
     super();
+    this.client = this.clientService.getClient();
   }
 
   async execute(args: string[], message: ChannelMessage) {
@@ -32,14 +36,32 @@ export class Ncc8Command extends CommandMessage {
 
       const textContent = `Go to `;
       try {
-        const url = await this.axiosClientService.get(
+        const res = await this.axiosClientService.get(
           `http://172.16.100.114:3000/ncc8/episode/${args[1]}`,
         );
-        if (!url) return;
+
+        // call api in sdk
+        // const data = await this.client.registerStreamingChannel({
+        //   clan_id: message.clan_id,
+        //   channel_id: this.clientConfigService.ncc8ChannelId,
+        // });
+
+        // const res = await this.axiosClientService.get(
+        //   'https://dev-mezon.nccsoft.vn:7305/v2/streaming-channels'
+        // );
+
+        // console.log(
+        //   'data',
+        //   message.clan_id,
+        //   this.clientConfigService.ncc8ChannelId,
+        //   data,
+        // );
+
+        if (!res) return;
 
         return this.replyMessageGenerate(
           {
-            messageContent: textContent + `# ${url?.data?.url}` ?? '',
+            messageContent: textContent + `# ${res?.data?.url}` ?? '',
             hg: [
               {
                 channelid: this.clientConfigService.ncc8ChannelId,
@@ -51,6 +73,12 @@ export class Ncc8Command extends CommandMessage {
           message,
         );
       } catch (error) {
+        console.log(
+          'error',
+          message.clan_id,
+          this.clientConfigService.ncc8ChannelId,
+          error,
+        );
         return this.replyMessageGenerate(
           {
             messageContent: 'Ncc8 not found',
