6f58feb41c02f8b973d187ba4611a4636f7b921a
man.maihong <man.maihong@ncc.asia>
2024-09-26 14:29:33 +0700

    off-machleo

--- src/bot/scheduler/meeting-scheduler.services.ts
+++ src/bot/scheduler/meeting-scheduler.services.ts
@@ -60,7 +60,7 @@ export class MeetingSchedulerService {
     return listVoiceChannelAvalable;
   }
 
-  @Cron(CronExpression.EVERY_MINUTE, { timeZone: 'Asia/Ho_Chi_Minh' })
+  // @Cron(CronExpression.EVERY_MINUTE, { timeZone: 'Asia/Ho_Chi_Minh' })
   async tagMeeting() {
     this.logger.warn(
       `time ${CronExpression.EVERY_MINUTE} for job tagMeeting to run!`,
@@ -378,7 +378,7 @@ export class MeetingSchedulerService {
     }
   }
 
-  @Cron(CronExpression.EVERY_MINUTE, { timeZone: 'Asia/Ho_Chi_Minh' })
+  // @Cron(CronExpression.EVERY_MINUTE, { timeZone: 'Asia/Ho_Chi_Minh' })
   async updateReminderMeeting() {
     this.logger.warn(
       `time ${CronExpression.EVERY_MINUTE} for job updateReminderMeeting to run!`,
