5707b5c707e1c56482faa58c0599de2e33380a1f
man2807 <73380926+man2807@users.noreply.github.com>
2024-10-14 09:45:22 +0700

    comment-wfh1 (#79)
    Co-authored-by: man.maihong <man.maihong@ncc.asia>

--- src/bot/scheduler/wfh-scheduler.service.ts
+++ src/bot/scheduler/wfh-scheduler.service.ts
@@ -30,7 +30,7 @@ export class WFHSchedulerService {
     this.client = clientService.getClient();
   }
 
-  @Cron('*/5 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
+  // @Cron('*/5 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
   async handlePingWFH() {
     try {
       if (await this.utilsService.checkHoliday()) return;
@@ -247,7 +247,7 @@ export class WFHSchedulerService {
     }
   }
 
-  @Cron('*/30 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
+  // @Cron('*/30 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
   async handlePingQuiz() {
     try {
       if (await this.utilsService.checkHoliday()) return;
