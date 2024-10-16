7ab24b34a24a47bca0039d93206626217e50e512
NguyenHoang <107922308+nvhoang291299@users.noreply.github.com>
2024-10-16 18:35:35 +0700

    update report wfh (#83)
    * update report wfh
    
    * format code

--- src/bot/utils/report-wfh.serivce.ts
+++ src/bot/utils/report-wfh.serivce.ts
@@ -1,88 +1,81 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { TABLE } from "src/bot/constants/table";
-import { WorkFromHome } from "src/bot/models/wfh.entity";
-import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
-import { UtilsService } from "../services/utils.services";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WorkFromHome } from 'src/bot/models/wfh.entity';
+import { Repository } from 'typeorm';
+import { UtilsService } from '../services/utils.services';
 
 @Injectable()
 export class ReportWFHService {
   constructor(
     @InjectRepository(WorkFromHome)
     private wfhRepository: Repository<WorkFromHome>,
-    // private komubotrestService: KomubotrestService,
-    private utilsService: UtilsService
+    private utilsService: UtilsService,
   ) {}
 
-  // async reportWfh(message: Message, args, client: Client) {
-  //   let authorId = message.author.id;
-  //   let fomatDate;
-  //   if (args[1]) {
-  //     const day = args[1].slice(0, 2);
-  //     const month = args[1].slice(3, 5);
-  //     const year = args[1].slice(6);
+  async reportWfh(message, args) {
+    let formatDate;
+    if (args[1]) {
+      const day = args[1].slice(0, 2);
+      const month = args[1].slice(3, 5);
+      const year = args[1].slice(6);
 
-  //     fomatDate = `${month}/${day}/${year}`;
-  //   } else {
-  //     fomatDate = new Date().toLocaleDateString("en-US", {
-  //       year: "numeric",
-  //       month: "2-digit",
-  //       day: "2-digit",
-  //     });
-  //   }
+      formatDate = `${month}/${day}/${year}`;
+    } else {
+      formatDate = new Date().toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      });
+    }
 
-  //   const wfhFullday = await this.wfhRepository
-  //     .createQueryBuilder("wfh")
-  //     .innerJoinAndSelect("komu_user", "m", "wfh.userId = m.userId")
-  //     .where(
-  //       '("status" = :statusACCEPT AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusACTIVE AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusAPPROVED AND pmconfirm = :pmconfirm AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay)',
-  //       {
-  //         type: "wfh",
-  //         statusACCEPT: "ACCEPT",
-  //         statusACTIVE: "ACTIVE",
-  //         statusAPPROVED: "APPROVED",
-  //         pmconfirm: false,
-  //         firstDay: this.utilsService
-  //           .getTimeToDayMention(fomatDate)
-  //           .firstDay.getTime(),
-  //         lastDay: this.utilsService
-  //           .getTimeToDayMention(fomatDate)
-  //           .lastDay.getTime(),
-  //       }
-  //     )
-  //     .groupBy("m.username")
-  //     .addGroupBy("wfh.userId")
-  //     .select("wfh.userId, COUNT(wfh.userId) as total, m.username")
-  //     .orderBy("total", "DESC")
-  //     .execute();
+    const wfhFullday = await this.wfhRepository
+      .createQueryBuilder('wfh')
+      .innerJoinAndSelect('komu_user', 'm', 'wfh.userId = m.userId')
+      .where(
+        '("status" = :statusACCEPT AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusACTIVE AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusAPPROVED AND pmconfirm = :pmconfirm AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay)',
+        {
+          type: 'wfh',
+          statusACCEPT: 'ACCEPT',
+          statusACTIVE: 'ACTIVE',
+          statusAPPROVED: 'APPROVED',
+          pmconfirm: false,
+          firstDay: this.utilsService
+            .getTimeToDayMention(formatDate)
+            .firstDay.getTime(),
+          lastDay: this.utilsService
+            .getTimeToDayMention(formatDate)
+            .lastDay.getTime(),
+        },
+      )
+      .groupBy('m.username')
+      .addGroupBy('wfh.userId')
+      .select('wfh.userId, COUNT(wfh.userId) as total, m.username')
+      .orderBy('total', 'DESC')
+      .execute();
 
-  //   let mess;
-  //   if (!wfhFullday) {
-  //     return;
-  //   } else if (Array.isArray(wfhFullday) && wfhFullday.length === 0) {
-  //     mess = "```" + "Không có ai vi phạm trong ngày" + "```";
-  //     return message.reply(mess).catch((err) => {
-  //       this.komubotrestService.sendErrorToDevTest(client, authorId, err);
-  //     });
-  //   } else {
-  //     for (let i = 0; i <= Math.ceil(wfhFullday.length / 50); i += 1) {
-  //       if (wfhFullday.slice(i * 50, (i + 1) * 50).length === 0) break;
-  //       mess = wfhFullday
-  //         .slice(i * 50, (i + 1) * 50)
-  //         .map((wfh) => `${wfh.username} - (${wfh.total})`)
-  //         .join("\n");
-  //       const Embed = new EmbedBuilder()
-  //         .setTitle(
-  //           "Những người bị phạt vì không trả lời wfh trong ngày hôm nay"
-  //         )
-  //         .setColor("Red")
-  //         .setDescription(`${mess}`);
-  //       return message.reply({ embeds: [Embed] }).catch((err) => {
-  //         this.komubotrestService.sendErrorToDevTest(client, authorId, err);
-  //       });
-  //     }
-  //   }
-  // }
+    let mess;
+    if (!wfhFullday) {
+      return;
+    } else if (Array.isArray(wfhFullday) && wfhFullday.length === 0) {
+      mess = ['```' + 'Không có ai vi phạm trong ngày' + '```'];
+      return mess;
+    } else {
+      const listMessage = [];
+      for (let i = 0; i <= Math.ceil(wfhFullday.length / 50); i += 1) {
+        if (wfhFullday.slice(i * 50, (i + 1) * 50).length === 0) break;
+        mess = wfhFullday
+          .slice(i * 50, (i + 1) * 50)
+          .map((wfh) => `${wfh.username} - (${wfh.total})`)
+          .join('\n');
+        listMessage.push(
+          'Những người bị phạt vì không trả lời wfh trong ngày hôm nay' +
+            '\n' +
+            `${mess}`,
+        );
+      }
+      return listMessage;
+    }
+  }
 
   // async reportCompalinWfh(message, args, client) {
   //   let authorId = message.author.id;
@@ -145,21 +138,21 @@ export class ReportWFHService {
   // }
 
   async reportMachleo(date: Date) {
-    const formatDate = date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
+    const formatDate = date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
     });
     const result = await this.wfhRepository
-      .createQueryBuilder("wfh")
-      .innerJoinAndSelect("komu_user", "m", "wfh.userId = m.userId")
+      .createQueryBuilder('wfh')
+      .innerJoinAndSelect('komu_user', 'm', 'wfh.userId = m.userId')
       .where(
         '("status" = :statusACCEPT AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusACTIVE AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay) OR ("status" = :statusAPPROVED AND pmconfirm = :pmconfirm AND "type" = :type AND wfh.createdAt >= :firstDay AND wfh.createdAt <= :lastDay)',
         {
-          type: "mention",
-          statusACCEPT: "ACCEPT",
-          statusACTIVE: "ACTIVE",
-          statusAPPROVED: "APPROVED",
+          type: 'mention',
+          statusACCEPT: 'ACCEPT',
+          statusACTIVE: 'ACTIVE',
+          statusAPPROVED: 'APPROVED',
           pmconfirm: false,
           firstDay: this.utilsService
             .getTimeToDayMention(formatDate)
@@ -167,12 +160,12 @@ export class ReportWFHService {
           lastDay: this.utilsService
             .getTimeToDayMention(formatDate)
             .lastDay.getTime(),
-        }
+        },
       )
-      .groupBy("m.email")
-      .addGroupBy("wfh.userId")
-      .select("m.email, COUNT(wfh.userId) as count")
-      .orderBy("count", "DESC")
+      .groupBy('m.email')
+      .addGroupBy('wfh.userId')
+      .select('m.email, COUNT(wfh.userId) as count')
+      .orderBy('count', 'DESC')
       .execute();
     return result;
   }
