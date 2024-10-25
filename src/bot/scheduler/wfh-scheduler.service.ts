c6061a8bd4ef8d579ec17bbea062dc85f72e18cf
man.maihong <man.maihong@ncc.asia>
2024-10-25 17:21:10 +0700

    change-time-send

--- src/bot/scheduler/wfh-scheduler.service.ts
+++ src/bot/scheduler/wfh-scheduler.service.ts
@@ -150,7 +150,7 @@ export class WFHSchedulerService {
   }
 
   async sendQuizzesWithLimit(userSend, botPing: boolean = true) {
-    const delay = 50;
+    const delay = 200;
     for (let i = 0; i < userSend.length; i++) {
       const user = userSend[i];
       await this.quizeService.sendQuizToSingleUser(user, botPing);
